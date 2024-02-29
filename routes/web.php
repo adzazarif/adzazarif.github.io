67ada759ce64dce87752ad5d6c16334362981de9
Adza Zarif Nur iskandar <adzazarf@gmail.com>
2024-02-29 14:54:05 +0700

    add logincontroller,view login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,10 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', [LoginController::class,'index'])->name('viewLogin');
+Route::post('/login', [LoginController::class,'authenticate'])->name('login');
 Route::get('/users', [UserController::class,'index'])->name('viewUser');
 Route::get('/detail-user/{name}/{email}/{address}', [UserController::class,'getDetail'])->name('getDetail');
 Route::post('/users', [UserController::class,'store'])->name('store');
 
-Route::get('/dashboard', [DashboardController::class,'index']);
+Route::get('/dashboard', [DashboardController::class,'index'])->name('dashboard');
 
 
